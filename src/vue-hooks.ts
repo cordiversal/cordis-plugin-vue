eb9b89cd1dd687142a6f6d3c6c9fb92828b5051d
TimeBather <open-source@timebather.cn>
2024-03-21 18:17:10 +0800

    fix: use general typings C extends Context instead of Context

--- src/vue-hooks.ts
+++ src/vue-hooks.ts
@@ -1,15 +1,15 @@
 import {inject as vueInject, onMounted, onUnmounted, provide, ref, Ref, ShallowRef, shallowRef, watch} from "vue";
 import {Context, ForkScope, Plugin} from "cordis";
-export function definePlugin(plugin:Plugin.Function,inject?:string[]){
-    let parent = vueInject<ShallowRef<Context|null>>('cordis/context');
-    let current:ShallowRef<Context|null> = shallowRef<Context|null>(null);
+export function definePlugin<C extends Context>(plugin:Plugin.Function<C>,inject?:string[]){
+    let parent = vueInject<ShallowRef<C|null>>('cordis/context');
+    let current:ShallowRef<C|null> = shallowRef<C|null>(null);
     if(!parent){
         throw new Error('Cannot get parent form vue: please ensure the component is in the app which injected context');
     }
     provide('cordis/context',current);
     let scope:ForkScope|null;
     onMounted(()=>{
-        const reloadHook = (parent:Context|null)=>{
+        const reloadHook = (parent:C|null)=>{
             if(scope){
                 scope.dispose();
                 scope = null;
